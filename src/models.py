e466646fcedf59fede4f3e92535b1458333f9dc1
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-05-10 21:18:19 +0000

    final v1

--- src/models.py
+++ src/models.py
@@ -9,33 +9,46 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'user'
-     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    firstname = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-class Posts(Base):
-    __tablename__ = 'post'
+class Planets(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    haircolor = Column(String(250), nullable=False)
+    skincolor = Column(String(250), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planets)
+
+
+class Favorites_Characters(Base):
+    __tablename__ = 'favorite_character'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(Users)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Characters)
 
-class Comments(Base):
-    __tablename__ = 'comment'
-   id = Column(Integer, primary_key=True)
+class Favorites_Planets(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(Users)
-    post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Posts)
-    comment_text = Column(String(250), nullable=False)
- 
-class Followers(Base):
-    __tablename__ = 'follower'
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
-    user_id_follower = Column(Integer, ForeignKey('user.id'))
-    user = relationship(Users)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planets)
 
     def to_dict(self):
         return {}
